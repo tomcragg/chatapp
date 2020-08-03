e868c5205f6d8be7dcc42dd2a0b4a180752c3a14
Tom Cragg <tom.cragg1@gmail.com>
2020-08-03 14:12:51 +0200

    Gravitar icon complete.

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcryptjs")
 const usersCollection = require("../db").db().collection("users")
 const validator = require("validator")
+const md5 = require("md5")
 
 let User = function (data) {
   this.data = data
@@ -79,6 +80,8 @@ User.prototype.login = function () {
       .findOne({ username: this.data.username })
       .then(attemptedUser => {
         if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
+          this.data = attemptedUser
+          this.getAvatar()
           resolve("Congrats!")
         } else {
           reject("Invalid username / password.")
@@ -103,11 +106,16 @@ User.prototype.register = function () {
       let salt = bcrypt.genSaltSync(10)
       this.data.password = bcrypt.hashSync(this.data.password, salt)
       await usersCollection.insertOne(this.data)
+      this.getAvatar()
       resolve()
     } else {
       reject(this.errors)
     }
   })
+}
+
+User.prototype.getAvatar = function () {
+  this.avatar = `https://s.gravatar.com/avatar/${md5(this.data.email)}?s=128`
 }
 
 module.exports = User
